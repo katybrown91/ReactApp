ae23845a14cf809d0135f3f588ddc3103ea83805
Katy Brown <thisis.katybrown@gmail.com>
2019-06-21 11:29:49 -0400

    updated

---
+++ my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div className="header">
+      <h1>Title</h1>
+      </div>
+      <div className = "footer">Contact Info</div>
+      
+    </div>
+  );
+}
+
+export default App;
