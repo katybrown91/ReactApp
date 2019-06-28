31042a5658d80aeb8907fa89fc00039b2f636c27
Katy Brown <thisis.katybrown@gmail.com>
2019-06-28 19:06:42 -0400

    update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Link, NavLink } from 'react-router-dom'
-import Home from './components/Home'
-import { Route, Switch } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import { Route, Switch } from 'react-router-dom';
+import Services from './components/Services';
 
 class App extends React.Component {
   render(){
   return (
 
     <div className="App">
+   
+        <Switch>
        <Route exact path='/' component={Home} /> 
+       <Route exact path='/services' component={Services} /> 
+       </Switch>
       </div>
       
       
--- my-react-app/src/components/Home.js
+++ my-react-app/src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
+import Services from './Services';
 
 class Home extends React.Component{
   render(){
     return(
-      <div></div>
+      <div>
+        <ul>
+          <li><NavLink to= "/services"></NavLink><h1>Services</h1> </li>
+        </ul>
+      </div>
     )
   }
 }
