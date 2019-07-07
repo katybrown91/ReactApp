a050c6bb9f7b3d84c1618ec4f9d0fedac059d51d
Katy Brown <thisis.katybrown@gmail.com>
2019-07-07 13:36:46 -0400

    about

--- my-react-app/src/components/About.js
+++ my-react-app/src/components/About.js
@@ -4,7 +4,47 @@ import Home from './Home';
 class About extends React.Component{
   render(){
     return(
-      <div></div>
+      <div>
+        <h2>About Us</h2>
+        <div className = "about">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nullam rhoncus sit amet dui id egestas. Etiam pellentesque tortor sed efficitur 
+          laoreet. Nam ut posuere velit, sit amet varius orci. Etiam bibendum quis nisi 
+          eget mollis. Sed dictum vitae elit quis sodales. Maecenas placerat augue a 
+          lacus vestibulum cursus. Nunc et pellentesque elit, id vehicula massa. 
+          Suspendisse tincidunt turpis eu pharetra elementum. Aliquam a pretium sapien. 
+          Duis urna metus, sollicitudin nec odio a, sagittis tincidunt elit. 
+          
+          Sed condimentum sapien sit amet molestie mollis. Sed non dui a ante aliquet 
+          viverra sit amet vitae justo. Duis ut nibh eget eros ultrices imperdiet. 
+          Phasellus hendrerit scelerisque metus. <br></br> <br></br>
+
+Integer cursus mi tellus, mollis ornare est commodo ut. Etiam consequat luctus tellus 
+id bibendum. Fusce tincidunt nulla non massa dictum, vitae vulputate mi sollicitudin. 
+Sed vitae arcu tristique, feugiat ligula ac, viverra erat. Sed scelerisque ornare 
+velit. Pellentesque laoreet volutpat nisi nec aliquam. Cras pretium interdum 
+suscipit. Orci varius natoque penatibus et magnis dis parturient montes, 
+nascetur ridiculus mus. Nunc tempor mi venenatis erat tristique efficitur. <br></br> <br></br>
+
+Pellentesque ultricies quam tristique enim commodo mattis. Nulla sit amet 
+imperdiet augue. Praesent facilisis magna in nisi bibendum, nec condimentum 
+risus suscipit. Integer sed mi quam. Vivamus suscipit pellentesque lacus in luctus. 
+Proin finibus enim erat, a vehicula mi rhoncus at. Proin viverra viverra quam vitae 
+venenatis. Proin quis feugiat arcu. Cras bibendum sagittis ligula, id faucibus 
+sem egestas ac. Nam at elit luctus, tincidunt ex non, auctor est. 
+Vivamus dictum arcu fermentum est egestas lobortis. Vivamus semper ultricies 
+felis ac euismod. Ut in pharetra neque, ut lobortis neque. Suspendisse id mi 
+quis orci pellentesque vulputate a quis est. <br></br> <br></br>
+
+Aenean ac vehicula neque. Duis sed consectetur ex. 
+Pellentesque nec leo est. In sem dolor, porta congue vulputate quis, 
+varius vitae eros. Nullam at lorem pulvinar libero vulputate accumsan. 
+Sed nec purus risus. Phasellus fringilla ac erat eu imperdiet. 
+Quisque malesuada magna eget porta dictum. Nullam egestas facilisis aliquet. 
+Integer tincidunt fermentum ante, quis auctor lectus sagittis ac. 
+Duis ultrices fermentum orci vel hendrerit.</p>
+</div>
+      </div>
     )
   }
 }
