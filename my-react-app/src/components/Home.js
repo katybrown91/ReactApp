af4e536195367706856d096968000c07954c247f
Katy Brown <thisis.katybrown@gmail.com>
2019-07-15 11:42:49 -0400

    images

--- my-react-app/src/components/Home.js
+++ my-react-app/src/components/Home.js
@@ -10,12 +10,16 @@ class Home extends React.Component{
   render(){
     return(
       <div>
-        <h1>Business Title</h1>
+        <div className="title">
+        <h1>Spa</h1>
+        </div>
         <nav>
         <Link to= "/services">Services</Link> <br></br>
         <Link to= "/about">About Us</Link>
         </nav>
-        <img src = {"./image/spaimage.jpg"}  alt= "spa" width="30%"/>
+        <img src = {"./image/spaimage.jpg"}  alt= "spa" width="250vw" height="250vh" />
+        <img src = {"./image/rocks.jpg"} alt="rocks" width= "200vw" height="250vh" />
+        <img src= {"./image/image3.jpg"} alt="flower with rocks" width="200vw" height="250vh" />
       </div>
     )
   }
